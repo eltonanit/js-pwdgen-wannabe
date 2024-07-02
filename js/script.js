8a06285ab4f9fbd9871782f09a72e455ddbcdbda
eltonanit <elton.ani@gmail.com>
2024-07-02 15:30:17 +0200

    [FIX] fixed

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.writeln('Messaggio')
+
+document.getElementById('subtitle').innerHTML = 'Progress';
+
+
+document.getElementById('text').innerText = 'Lorem ipsum';
+
+//VARIABLE
+
+let first_name = prompt('inserisci il tuo nome ');
+let second_name = prompt('inserisci il tuo cognome ');
+let color_name = prompt('inserisci il tuo colore preferito ');
+
+
+document.getElementById('first_string').innerText = first_name + ' ' + second_name;
+
+const string = first_name;
+console.log(string);
+
+let full_string = `Il tuo nome e cognome  ${first_name} ${second_name} il tuo colore preferito`;
+
+
